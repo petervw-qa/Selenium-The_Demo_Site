350abd96f4683221ae81b462b24adb087e43b48e
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-02-15 15:03:57 +0000

    automated testing using selenium for the demo site task

---
+++ src/test/java/test/DemoSiteTest.java
@@ -0,0 +1,75 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class DemoSiteTest {
+
+	private static RemoteWebDriver driver;
+	private WebElement targ;
+
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+		driver = new ChromeDriver(chromeCfg());
+		driver.manage().window().setSize(new Dimension(1366, 768));
+
+	}
+
+	@Test
+	public void test() throws InterruptedException {
+		driver.get("http://thedemosite.co.uk/");
+
+		targ = driver.findElement(By.xpath("/html/body/div/center/table/tbody/tr[2]/td/div/center/table/tbody/tr/td[2]/p/small/a[3]"));
+		targ.click();
+		targ = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[1]/td[2]/p/input"));
+		targ.sendKeys("guest");
+		targ = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[2]/td[2]/p/input"));
+		targ.sendKeys("guest");
+		targ = driver.findElement(By.xpath("/html/body/div/center/table/tbody/tr[2]/td/div/center/table/tbody/tr/td[2]/p/small/a[4]"));
+		targ.click();
+		targ = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[1]/td[2]/p/input"));
+		targ.sendKeys("guest");
+		targ = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[2]/td[2]/p/input"));
+		targ.sendKeys("guest");
+		targ = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/table/tbody/tr[3]/td[2]/p/input"));
+		targ.click();
+		
+		assertEquals("**Successful Login**", driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/big/blockquote/blockquote/font/center/b")).getText());
+		
+		Thread.sleep(10000);
+	}
+
+	@After
+	public void tearDown() {
+		driver.close();
+	}
+
+	public static ChromeOptions chromeCfg() {
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		ChromeOptions cOptions = new ChromeOptions();
+
+		// Settings
+		prefs.put("profile.default_content_setting_values.cookies", 2);
+		prefs.put("network.cookie.cookieBehavior", 2);
+		prefs.put("profile.block_third_party_cookies", true);
+
+		// Create ChromeOptions to disable Cookies pop-up
+		cOptions.setExperimentalOption("prefs", prefs);
+
+		return cOptions;
+	}
+
+}
